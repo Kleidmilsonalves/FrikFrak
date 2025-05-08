cecb8d63c9f7fac6d269d10b889036df76fe9226
Kleidmilson <148908445+Kleidmilsonalves@users.noreply.github.com>
2025-05-08 14:49:13 -0100

    Add files via upload

---
+++ CAAFrikFrak.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.caafrikfrak;
+
+/**
+ *
+ * @author Kleidmilson Alves
+ */
+public class CAAFrikFrak {
+    
+    
+    
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
